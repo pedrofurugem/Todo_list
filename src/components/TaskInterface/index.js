5e567569bee51c984a737bf00d3e7ee598c10107
Pedro Roberto <pedrofuruguem@hotmail.com>
2023-10-27 16:16:40 -0300

    HTML e CSS 'prontos'

--- src/components/TaskInterface/index.js
+++ src/components/TaskInterface/index.js
@@ -6,6 +6,11 @@ import {
   TitleBox,
   Description,
   RigthContainer,
+  TitleRightContainer,
+  DescRightContainer,
+  TaskBox,
+  Task,
+  InputTask
 } from './style.js'
 
 import Logo from '../../images/logo.png'
@@ -22,13 +27,23 @@ export const TaskInterface = () => {
               <Description>Adicione ao lado as tarefas que deseja fazer hoje e não pare até ela estar vazia!</Description>
             </LeftContainer>
             <RigthContainer>
-              <h1>Minhas Tarefas</h1>
-              <p>Tarefas do dia...</p>
-              <ul>
-                <li>Tarefa 1</li>
-                <li>Tarefa 2</li>
-              </ul>
-              <input type="text" placeholder="Digite sua próxima tarefa aqui... "/>
+              <TitleRightContainer>Minhas Tarefas</TitleRightContainer>
+              <DescRightContainer>Tarefas do dia...</DescRightContainer>
+                <TaskBox>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M7.49999 9.31826L12.9548 14.7731L14.7731 12.9548L9.31826 7.49998L14.7731 2.04516L12.9548 0.226886L7.49999 5.68171L2.04516 0.226887L0.226888 2.04516L5.68171 7.49999L0.226888 12.9548L2.04516 14.7731L7.49999 9.31826ZM7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
+                  <path d="M7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
+                  </svg>
+                  <Task>Tarefa 1</Task>
+                </TaskBox>
+                <TaskBox>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M7.49999 9.31826L12.9548 14.7731L14.7731 12.9548L9.31826 7.49998L14.7731 2.04516L12.9548 0.226886L7.49999 5.68171L2.04516 0.226887L0.226888 2.04516L5.68171 7.49999L0.226888 12.9548L2.04516 14.7731L7.49999 9.31826ZM7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
+                  <path d="M7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
+                  </svg>
+                  <Task>Tarefa 2</Task>
+                </TaskBox>
+              <InputTask type="text" placeholder="Digite sua próxima tarefa aqui... "/>
             </RigthContainer>
           </Container>
         </MainContainer>
--- src/components/TaskInterface/style.js
+++ src/components/TaskInterface/style.js
@@ -60,3 +60,60 @@ export const RigthContainer = styled.div`
     background: #FFF;
     box-shadow: 0px 40px 100px 0px rgba(0, 0, 0, 0.08), 0px 10px 80px 0px rgba(0, 0, 0, 0.06), 0px 2px 15px 0px rgba(0, 0, 0, 0.12), 0px 2px 4px 0px rgba(0, 0, 0, 0.06);
 `
+
+export const TitleRightContainer = styled.h1`
+    color: #35383E;
+    font-size: 40px;
+    font-weight: 800;
+`
+export const DescRightContainer = styled.p`
+    color: rgba(53, 56, 62, 0.50);
+    font-size: 20px;
+    font-weight: 700;
+`
+
+export const TaskBox = styled.div`
+    display: flex;
+    width: 410px;
+    padding: 15px;
+    align-items: center;
+    gap: 15px;
+
+    border-radius: 22.5px;
+    background: rgba(53, 56, 62, 0.05);
+
+    svg {
+        display: flex;
+        width: 35px;
+        height: 35px;
+        padding: 10px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex-shrink: 0;
+        border-radius: 11.25px;
+        background: rgba(53, 56, 62, 0.10);
+        cursor: pointer;
+    }
+`
+
+export const Task = styled.p`
+    color: #35383E;
+    font-size: 20px;
+    font-weight: 700;
+`
+
+export const InputTask = styled.input`
+    display: flex;
+    padding: 15px 15px 10px 15px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 5px;
+    align-self: stretch;
+    //border-radius: 22.5px;
+
+    color: rgba(53, 56, 62, 0.50);
+    font-size: 22px;
+    font-weight: 800;
+`
