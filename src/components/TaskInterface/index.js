817c21cffb969255be5f8217700860f7c752cfd0
Pedro Roberto <pedrofuruguem@hotmail.com>
2023-11-06 16:13:30 -0300

    funçoes add e remove

--- src/components/TaskInterface/index.js
+++ src/components/TaskInterface/index.js
@@ -31,7 +31,6 @@ export const TaskInterface = () => {
   
   function removeTask(id){
     setTasks(tasks.filter((task) => task.id !== id))
-    console.log("tarfeda removida " + id)
   }
 
   return(
@@ -48,8 +47,8 @@ export const TaskInterface = () => {
           <TitleRightContainer>Minhas Tarefas</TitleRightContainer>
           <DescRightContainer>Tarefas do dia...</DescRightContainer>
           { tasks.map((task) => ( 
-            <TaskBox key={task.id} onClick = { ()=> removeTask(task.id)}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+            <TaskBox key={task.id}>
+              <svg onClick = { ()=> removeTask(task.id)} xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M7.49999 9.31826L12.9548 14.7731L14.7731 12.9548L9.31826 7.49998L14.7731 2.04516L12.9548 0.226886L7.49999 5.68171L2.04516 0.226887L0.226888 2.04516L5.68171 7.49999L0.226888 12.9548L2.04516 14.7731L7.49999 9.31826ZM7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
                 <path d="M7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
               </svg>
