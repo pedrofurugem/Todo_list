2d7c8d6813e0998e13456d4a900c37b8d2e67b33
Pedro Roberto <pedrofuruguem@hotmail.com>
2023-11-10 15:57:24 -0300

    componentização

---
+++ src/components/Task/style.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const TaskBox = styled.div`
+    display: flex;
+    width: 410px;
+    padding: 15px;
+    align-items: center;
+    gap: 15px;
+
+    border-radius: 22.5px;
+    background: rgba(53, 56, 62, 0.05);
+
+    svg {
+        display: flex;
+        width: 35px;
+        height: 35px;
+        padding: 10px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex-shrink: 0;
+        border-radius: 11.25px;
+        background: rgba(53, 56, 62, 0.10);
+        cursor: pointer;
+    }
+`
+
+export const Task = styled.p`
+    color: #35383E;
+    font-size: 20px;
+    font-weight: 700;
+`
--- src/components/TaskInterface/index.js
+++ src/components/TaskInterface/index.js
@@ -1,6 +1,8 @@
 import { useState, useRef, useEffect } from 'react'
 import axios from 'axios';
 
+import TaskBox from '../Task/index.js'
+
 import { 
   MainContainer, 
   Container,
@@ -11,18 +13,16 @@ import {
   RigthContainer,
   TitleRightContainer,
   DescRightContainer,
-  TaskBox,
-  Task,
   InputTask
 } from './style.js'
 
 import Logo from '../../images/logo.png'
-//https://plataforma.universodev.com.br/api/todolist/
+
 export const TaskInterface = () => {
   const [tasks, setTasks] = useState([{ id: Math.random(), text: 'Tarefa 1'}, { id: Math.random(), text: 'Tarefa 2'}])
   
   const taskText = useRef("") 
-
+  
   useEffect(()=> {
     const fetchData = async ()=> {
       const {data: taskList} = await axios.get('https://plataforma.universodev.com.br/api/todolist/')
@@ -30,7 +30,6 @@ export const TaskInterface = () => {
     }
     fetchData()
   }, [tasks])
-
   
   async function insertNewTask(event){
     if(event.key === "Enter"){
@@ -38,11 +37,6 @@ export const TaskInterface = () => {
       setTasks([...tasks, {id: newTaskInfo.id, text: newTaskInfo.text }])
       taskText.current.value = ""
     }
-  }
-  
-  async function removeTask(id){
-    const data = await axios.delete(`https://plataforma.universodev.com.br/api/todolist/${id}`)
-    setTasks(tasks.filter((task) => task.id !== id))
   }
 
   return(
@@ -59,14 +53,8 @@ export const TaskInterface = () => {
           <TitleRightContainer>Minhas Tarefas</TitleRightContainer>
           <DescRightContainer>Tarefas do dia...</DescRightContainer>
           { tasks.map((task) => ( 
-            <TaskBox key={task.id}>
-              <svg onClick = { ()=> removeTask(task.id)} xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M7.49999 9.31826L12.9548 14.7731L14.7731 12.9548L9.31826 7.49998L14.7731 2.04516L12.9548 0.226886L7.49999 5.68171L2.04516 0.226887L0.226888 2.04516L5.68171 7.49999L0.226888 12.9548L2.04516 14.7731L7.49999 9.31826ZM7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
-                <path d="M7.49999 9.31826L9.31826 7.49998L7.49999 5.68171L5.68171 7.49999L7.49999 9.31826Z" fill="#35383E" fill-opacity="0.25"/>
-              </svg>
-              <Task>{task.text}</Task>
-            </TaskBox>
-          )) }
+            <TaskBox id={task.id} text={task.text} updateTasks={setTasks}/>
+          ))}
           <InputTask type="text"
                       placeholder="Digite sua próxima tarefa aqui... "
                       onKeyDown={insertNewTask}
